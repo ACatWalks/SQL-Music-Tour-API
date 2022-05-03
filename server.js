a00326681c9140ed61921074cd46cb46067700d9
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-05-02 20:49:36 -0700

    Connection successful, have started on next activity

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 // DEPENDENCIES
 const express = require('express')
 const app = express()
+const {Sequelize} = require('sequelize')
 
 // CONFIGURATION / MIDDLEWARE
 require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
+
+//SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI, 'postgres', 'JAAEW314159jaaew', {
+    host: 'localhost',
+    dialect: 'postgres'
+});
+
+try{
+    sequelize.authenticate();
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
+} catch(err){
+    console.log(`Unable to connect to PG: ${err}`)
+}
 
 // ROOT
 app.get('/', (req, res) => {
@@ -18,3 +32,8 @@ app.get('/', (req, res) => {
 app.listen(process.env.PORT, () => {
     console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
 })
+
+//const sequelize = new Sequelize('database', 'username', 'password', {
+    //host: 'localhost',
+    //dialect: /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */
+  //});
