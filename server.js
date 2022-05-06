5e9a90d4b0170a159d1714d2d7ccc494a8219297
Amanda Woods <Amanda@Amandas-MacBook-Pro.local>
2022-05-06 14:51:30 -0700

    All controllers are present

--- server.js
+++ server.js
@@ -8,8 +8,23 @@ require('dotenv').config()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+//CONTROLLERS
+const bandsController = require('./controllers/bands_controller');
+app.use('/bands', bandsController);
+const stagesController = require('./controllers/stages_controller');
+app.use('/stages', stagesController);
+const eventsController = require('./controllers/events_controller');
+app.use('/events', eventsController);
+
 //SEQUELIZE CONNECTION
-const sequelize = new Sequelize(process.env.PG_URI, 'postgres', 'JAAEW314159jaaew', {
+/*const sequelize = new Sequelize(process.env.PG_URI, 'postgres', 'JAAEW314159jaaew', {
     host: 'localhost',
     dialect: 'postgres'
 });
@@ -19,14 +34,7 @@ try{
     console.log(`Connected with Sequelize at ${process.env.PG_URI}`)
 } catch(err){
     console.log(`Unable to connect to PG: ${err}`)
-}
-
-// ROOT
-app.get('/', (req, res) => {
-    res.status(200).json({
-        message: 'Welcome to the Tour API'
-    })
-})
+}*/
 
 // LISTEN
 app.listen(process.env.PORT, () => {
